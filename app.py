08596992503d8153185e31b650990562430252a2
leo <leo.avril1908@gmail.com>
2024-02-06 19:52:25 +0100

    added .gitignore

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from main import SpotifyExplorer  # import the SpotifyExplorer class from your main.py file
+# import the SpotifyExplorer class from your main.py file
+from main import SpotifyExplorer
 import os
 import json
 import pandas as pd
@@ -14,17 +15,21 @@ pid = 1000000
 # Print all the tracks that have been sugested
 tracks_embed = []
 
-spotify_explorer = SpotifyExplorer(numFiles=0, retrainNNC=False)  # initialize the SpotifyExplorer
+# initialize the SpotifyExplorer
+spotify_explorer = SpotifyExplorer(numFiles=0, retrainNNC=False)
 
 # Set up Spotipy
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="9858097f4b8c4a3dbec46bbc0cf04186",
-                                                           client_secret="d6c0234cad074c45bc3baf0e56c6b4bb"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f2458f8ee1304f43acbb7f6037de86c8",
+                                                           client_secret="cd9f1486ebad4d088766160b532d04de"))
+
 
 @app.route('/')
 def index():
-    tracks_embed = request.args.getlist('tracks_embed')  # Get list from query parameters
+    tracks_embed = request.args.getlist(
+        'tracks_embed')  # Get list from query parameters
     print(tracks_embed)
     return render_template('index.html', tracks_embed=tracks_embed)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -55,31 +60,29 @@ def predict():
         'album_name': track['track']['album']['name']
     } for i, track in enumerate(raw_tracks)]
 
-    
     # Create a suitable JSON dictionary
     playlist = {
         'info': {
-            'generated_on': '2017-12-04 03:05:11.774401', 
-            'slice': f'{pid}-{pid+999}', 
+            'generated_on': '2017-12-04 03:05:11.774401',
+            'slice': f'{pid}-{pid+999}',
             'version': 'v1'
         },
         'playlists': [{
             'name': playlist_details['name'],
             'collaborative': playlist_details['collaborative'],
             'pid': pid,
-            'modified_at': 1468800000,  
+            'modified_at': 1468800000,
             'num_tracks': len(tracks),
             'num_albums': len(set([track['album_uri'] for track in tracks])),
             'num_followers': playlist_details['followers']['total'],
             'tracks': tracks,
-            'num_edits': 2,  
+            'num_edits': 2,
             'duration_ms': sum([track['duration_ms'] for track in tracks]),
             'num_artists': len(set([track['artist_uri'] for track in tracks]))
         }]
     }
-    
-    # create pandas Series object
 
+    # create pandas Series object
 
     playlist_series = pd.Series({
         'name': playlist_details['name'],
@@ -108,14 +111,12 @@ def predict():
     pid += 1000
 
     # Make a prediction and get the embed links of the suggested tracks
-    
+
     print(playlist_series)
-    tracks_embed  = spotify_explorer.PredictPlaylist(playlist_series)
-    # Redirect to the index page and pass the embed_link to the template   
+    tracks_embed = spotify_explorer.PredictPlaylist(playlist_series)
+    # Redirect to the index page and pass the embed_link to the template
     return redirect(url_for('index', tracks_embed=tracks_embed))
 
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
