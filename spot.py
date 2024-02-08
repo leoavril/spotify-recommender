ac0918e4aa72cad84f99b474953f8669cbfe0413
leo <leo.avril1908@gmail.com>
2024-02-08 03:21:15 +0100

    modified requirements file

--- spot.py
+++ spot.py
@@ -1,38 +1,44 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+spotify = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials())
 
 SPOTIFY_CLIENT = '9858097f4b8c4a3dbec46bbc0cf04186'
 SPOTIFY_CLIENT_SECRET = 'd6c0234cad074c45bc3baf0e56c6b4bb'
 SPOTIFY_REDIRECT_URI = 'http://localhost:5000/callback'
 
+
 def createRandomPredictionsDF(self, numInstances):
-        print(f"Generating {numInstances} data points")
-        data = [self.displayRandomPrediction() for _ in tqdm(range(numInstances))]
-        df = pd.DataFrame(data)
-        df.to_csv("predictionData.csv")
+    print(f"Generating {numInstances} data points")
+    data = [self.displayRandomPrediction() for _ in tqdm(range(numInstances))]
+    df = pd.DataFrame(data)
+    df.to_csv("predictionData.csv")
 
-    # Display prediction for given playlist and return as dictionary
+# Display prediction for given playlist and return as dictionary
 
-    def displayRandomPrediction(self):
+
+def displayRandomPrediction(self):
+    playlist = self.getRandomPlaylist()
+    while len(playlist["tracks"]) < 10:
         playlist = self.getRandomPlaylist()
-        while len(playlist["tracks"]) < 10:
-            playlist = self.getRandomPlaylist()
 
-        predictions = self.predictNeighbour(playlist=playlist,
-            numPredictions=50,
-            songs=self.songs)
+    predictions = self.predictNeighbour(playlist=playlist,
+                                        numPredictions=50,
+                                        songs=self.songs)
 
-        playlistName = playlist["name"]
-        playlist = [getTrackandArtist(trackURI, self.songs) for trackURI in playlist["tracks"]]
-        predictions = [getTrackandArtist(trackURI, self.songs) for trackURI in predictions]
-        return {
-            "name": playlistName,
-            "playlist": playlist,
-            "predictions": predictions
-        }
-    
+    playlistName = playlist["name"]
+    playlist = [getTrackandArtist(trackURI, self.songs)
+                for trackURI in playlist["tracks"]]
+    predictions = [getTrackandArtist(trackURI, self.songs)
+                   for trackURI in predictions]
+    return {
+        "name": playlistName,
+        "playlist": playlist,
+        "predictions": predictions
+    }
+
+
 """
     def evalAccuracy(self, numPlaylists, percentToObscure=0.5): 
         print()
@@ -57,4 +63,3 @@ def createRandomPredictionsDF(self, numInstances):
         accuracies = [getAcc(percentToObscure) for _ in tqdm(range(numPlaylists))]
         avgAcc = round(sum(accuracies) / len(accuracies), 4) * 100
         print(f"Using {self.classifier.name}, we predicted {avgAcc}% of obscured songs")"""
-    
